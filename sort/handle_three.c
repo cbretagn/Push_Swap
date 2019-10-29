348f5867526313ceed9d2141f74db9bd3faef729
cgaspart <cgaspart@student.42.fr>
2019-10-29 22:27:16 +0100

    handle_three + todo checker no arg

---
+++ sort/handle_three.c
@@ -0,0 +1,24 @@
+
+//123 na
+//132 ra sa rra
+//213 sa
+//231 ra sa rra sa
+//321 sa ra sa rra sa
+//312 sa ra sa rra
+
+//add three fun to handle long ones and done
+void handle_three(t_pile *pl, t_pile *instru)
+{
+    if (a < b && b < c && a < c)
+        ;
+    else if (a < b && b > c && a < c)
+        ;
+    else if (a > b && b < c && a < c)
+        ;
+    else if (a < b && b > c && a > c)
+        ;
+    else if (a > b && b > c && a > c)
+        ;
+    else
+        ;
+}
